e0be82696d6939353962950d678d6618812707f4
Sushavan Paul <sushavan.paul.524@gmail.com>
2020-08-28 09:14:14 +0530

    wrong code

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './collection.styles.scss';
+
+const CollectionPage = ({match}) => (
+  <div className = 'collection-page'>
+    <h2 className = 'title'>PAGE</h2>
+  </div>
+);
+
+
+export default CollectionPage;
