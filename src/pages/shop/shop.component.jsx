303c0f83484bfd55fa97fc5a801c4edd4eb6ae3b
Sushavan Paul <sushavan.paul.524@gmail.com>
2020-08-28 09:19:36 +0530

    wrong code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {Route} from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+const ShopPage = ({match}) => (
+  <div className = 'shop-page'>
+	<Route exact path = {`${match.path}`} component = {CollectionsOverview} />
+	<Route path = {`${match.path}/:categoryId`} component = {CollectionPage} />
+  </div>
+);
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
-import './collection.styles.scss';
-
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
-  return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default ShopPage;
