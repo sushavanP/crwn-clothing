ab7a605653bd8945f94837e63b9d64f121081b74
Sushavan Paul <sushavan.paul.524@gmail.com>
2020-08-28 20:21:29 +0530

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,8 +5,8 @@ import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({match}) => (
   <div className = 'shop-page'>
-	<Route exact path = {`${match.path}`} component = {CollectionsOverview} />
-	<Route path = {`${match.path}/:categoryId`} component = {CollectionPage} />
+	  <Route exact path = {`${match.path}`} component = {CollectionsOverview} />
+	  <Route path = {`${match.path}/:categoryId`} component = {CollectionPage} />
   </div>
 );
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,24 @@
-import {createSelector} from 'reselect';
+import { createSelector } from "reselect";
 
-const selectShop = state => state.shop;
+const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
-  shop => shop.collections
+  (shop) => shop.collections
 );
 
-export const selectCollection = collectionUrlParam =>
-  createSelector(
-    [selectCollections],
-    collections => collections[collectionUrlParam]
-  );
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam => createSelector(
+  [selectCollections],
+  collections => collections[collectionUrlParam]
+);
+
+/*
+    I have converted the shop data into an object thus the above code which is also followed by Yihua,
+    can be used. If I want to make it run using the array shop data which was the original form, 
+    then check codesandbox where yousaf helped me.
+*/
