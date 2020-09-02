a20a859f3bd19f909a812084ef001fbedcf16435
Sushavan Paul <sushavan.paul.524@gmail.com>
2020-09-02 20:22:41 +0530

    prettier

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
+import React from "react";
+import { Route } from "react-router-dom";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({match}) => (
-  <div className = 'shop-page'>
-	  <Route exact path = {`${match.path}`} component = {CollectionsOverview} />
-	  <Route path = {`${match.path}/:categoryId`} component = {CollectionPage} />
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
   </div>
 );
 
